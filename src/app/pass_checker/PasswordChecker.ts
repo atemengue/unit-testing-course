381ab04e41a4f19a46bd7b7f38eab12dfe6bc1cd
atemengue <moafembe@gmail.com>
2025-02-22 22:25:54 +0100

    TDD OK

--- src/app/pass_checker/PasswordChecker.ts
+++ src/app/pass_checker/PasswordChecker.ts
@@ -1,44 +1,67 @@
 
+export enum PasswordErros {
+  SHORT = "Password is too short!",
+  NO_UPPER_CASE = "Upper case letter required!",
+  NO_LOWERCASE = "Lower case letter required!",
+  NO_NUMBER = "At least one number required!"
+}
+
+
+export interface IResult {
+  isValid: boolean,
+  reasons: string[]
+}
+
 
 export class PasswordChecker {
-  public checkPassword(password: string): boolean {
+  public checkPassword(password: string): IResult {
 
-    let isValid: boolean = false;
+    const reasons : string[] = [];
 
+    let result = {
+      isValid: false,
+      reasons: reasons
+    }
 
-    isValid = this.checkForLength(password) 
-    && this.checkForNumber(password)
-    && this.checkLoweCase(password)
-    && this.checkUpperCase(password)
+    result.isValid = this.checkForLength(password, reasons) 
+    && this.checkForNumber(password, reasons)
+    && this.checkLoweCase(password, reasons)
+    && this.checkUpperCase(password, reasons)
     
-    return isValid;
+    return result;
 
   }
 
 
-  private checkForLength(password: string) {
+  private checkForLength(password: string, reasons: string[]) {
     if(password.length < 8) {
+      reasons.push(PasswordErros.SHORT);
       return false
+      
     };
+    
     return true;
   }
 
-  private checkForNumber(str: string) {
+  private checkForNumber(str: string, reasons: string[]) {
     const hasNumber = /\d/; 
     if(!hasNumber.test(str)){
+      reasons.push(PasswordErros.NO_NUMBER)
       return false
     }
     return true;
   }
 
-  private checkLoweCase(password: string) {
-    if(password === password.toLowerCase()){
+  private checkLoweCase(password: string, reasons: string[]) {
+    if(password === password.toUpperCase()){
+      reasons.push(PasswordErros.NO_LOWERCASE);
       return false;
     }
     return true;
   }
-  private checkUpperCase(password: string){
-    if(password === password.toUpperCase()){
+  private checkUpperCase(password: string, reasons: string[]){
+    if(password === password.toLowerCase()){
+      reasons.push(PasswordErros.NO_UPPER_CASE);
       return false
     }
     return true;
--- tests/pass_checker/PasswordChecker.test.ts
+++ tests/pass_checker/PasswordChecker.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { PasswordChecker } from '../../src/app/pass_checker/PasswordChecker';
+import { PasswordChecker, PasswordErros } from '../../src/app/pass_checker/PasswordChecker';
 
 
 describe('Password Checker Suite Test', () => {
@@ -10,42 +10,48 @@ describe('Password Checker Suite Test', () => {
     SUT = new PasswordChecker();
   })
 
-  it("Should return false false it password is empty", () => {
+  it("Should return false it password is empty", () => {
     const password = "";
     const expect_result = SUT.checkPassword(password);
-    expect(expect_result).toBe(false);
-
+    expect(expect_result.isValid).toBe(false);
+    expect(expect_result.reasons).toContain("Password is too short!")
+    
   });
-
+  
   it("Should return false if password length less than 8", () => {
     const password = "123adda";
     const expect_result = SUT.checkPassword(password);
-    expect(expect_result).toBe(false);
+    expect(expect_result.isValid).toBe(false);
+    expect(expect_result.reasons).toContain(PasswordErros.SHORT)
 
   });
 
   it("Should return  false if password has no upper cas letter",() => {
     const password = "1234abcde";
     const expect_result = SUT.checkPassword(password);
-    expect(expect_result).toBe(false);
+    expect(expect_result.isValid).toBe(false);
+    expect(expect_result.reasons).toContain(PasswordErros.NO_UPPER_CASE)
   });
 
   it("Should return false if password has no lowercase cas letter",() => {
     const password = "1234ABCDE";
     const expect_result = SUT.checkPassword(password);
-    expect(expect_result).toBe(false);
+    expect(expect_result.isValid).toBe(false);
+    expect(expect_result.reasons).toContain(PasswordErros.NO_LOWERCASE)
   });
 
   it("Should return true if password is correct", () => {
     const password = "1234ADecf";
     const expect_result = SUT.checkPassword(password);
-    expect(expect_result).toBe(true);
+    expect(expect_result.isValid).toBe(true);
+    expect(expect_result.reasons.length).toBe(0)
   });
 
   it("Should return false if password has not number", () => {
     const password = "abcdABCD";
     const expect_result = SUT.checkPassword(password);
-    expect(expect_result).toBe(false);
+    expect(expect_result.isValid).toBe(false);
+    expect(expect_result.reasons).toContain(PasswordErros.NO_NUMBER);
   })
 
 })
