376c5ea780bcb471f0856f6f7f615e52e169d047
atemengue <moafembe@gmail.com>
2025-03-31 01:22:04 +0100

    asyn one

---
+++ src/app/async/async.js
@@ -0,0 +1,48 @@
+
+async function isValidUserName(userName) {
+  // Placeholder for request checking if username is valid
+  if (!userName || !userName.includes('@')) {
+      return false;
+  }
+  else{
+      return true;
+  }
+}
+
+async function createAccount(username) {
+  if (!isValidUserName(username)) {
+      throw exception.InvalidUsernameError("Please enter a valid username")
+  }
+  const userExists = await users.userExists(username);
+  return new Promise((resolve, reject) => {
+      if (!userExists) {
+          resolve({data: {
+              "userId": users.createUserId(),
+              "username": username,
+          }}) 
+      } else {
+          reject("User already exists")
+      }
+     
+  })
+}
+
+export function fetchData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const data = [1, 2, 3];
+      resolve(data);
+    })
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ tests/async/async.test.js
@@ -0,0 +1,15 @@
+import { describe, expect, it } from 'vitest';
+
+import { fetchData } from '../../src/app/async/async';
+
+
+
+describe("Async Code Testing", () => {
+
+  it("Should should return a promise a resolve an array of numbers", async () => {
+      const result = await fetchData();
+      expect(Array.isArray(result)).toBeTruthy();
+      expect(Array.length).toBeGreaterThan(0);
+  })
+
+})
