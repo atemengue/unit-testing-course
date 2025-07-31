742f5b6afa0188ae8ec1780e9391fb10bf415ebf
atemengue <moafembe@gmail.com>
2025-07-31 07:43:09 +0100

    feat: start category service

---
+++ tests/services/category.service.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it } from 'vitest';
+
+
+describe("Category Service", () => {
+
+  describe("listeCategories tests suites", () => {
+    it.todo("doit me retourner une liste de categories de cafe");
+  });
+
+
+  describe("createCategories tests suites", () => {
+    it.todo("doit creer une categorie de cafe")
+  })
+
+})
