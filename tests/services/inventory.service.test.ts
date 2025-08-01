e5874200e1ccf68d13afb99a0feb1c19a6d0a865
atemengue <moafembe@gmail.com>
2025-08-01 07:02:14 +0100

    feat: setup and tearm inventory

---
+++ tests/services/inventory.service.test.ts
@@ -0,0 +1,56 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import Inventory from '../../src/models/inventory';
+import { CheckInterface, InventoryService } from '../../src/services/inventory.service';
+
+
+vi.mock("../../src/models/inventory.ts", () => {
+  return {
+    default: {
+      findOne: vi.fn()
+    }
+  }
+} )
+
+
+describe("Inventory Service", () => {
+
+  let sut: InventoryService;
+  beforeEach(() => {
+    sut  = new InventoryService();
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  })
+
+  describe("checkInventory Tests Suites", () => {
+
+    it("doit me retourner un objet avec isAvailable = false et message =`product Not Found`", async () => {
+
+      // Arrange
+      const id  = "1";
+      const expected = {
+        message: 'Product Not Found',
+        isAvailable:  false
+      };
+
+      // Mock
+      (Inventory.findOne as any).mockResolvedValue(null)
+
+      // Act
+      const actual =  await sut.checkInventory(id);
+
+      // Assert
+      expect(Inventory.findOne).toHaveBeenCalledWith({"productId": id });
+      expect(Inventory.findOne).toHaveBeenCalledTimes(1);
+      expect(actual).toEqual(expected);
+      expect(actual.message).toMatch(/Not Found/i);
+      expect(actual.isAvailable).toBeFalsy();
+
+    });
+    it.todo("doit me retourner un objet avec quantite et isAvailable = true ");
+    it.todo("doit me retourner un message d'erreur");
+
+  })
+
+})
