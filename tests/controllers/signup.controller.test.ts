0112d524219344d2dae4d13f91175821fae7073e
atemengue <moafembe@gmail.com>
2025-08-05 10:33:06 +0100

    assert test case 01

--- tests/controllers/signup.controller.test.ts
+++ tests/controllers/signup.controller.test.ts
@@ -1,7 +1,9 @@
 import { Request } from 'express';
+import JWT from 'jsonwebtoken';
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import SignUpController from '../../src/controllers/signup.controller';
 import User from '../../src/models/user';
+import { hashed } from '../../src/utils/hashed-password';
 import { verify } from '../../src/utils/verify';
 
 
@@ -27,7 +29,7 @@ vi.mock(import("jsonwebtoken"), async (importOriginal) => {
   const actual = await importOriginal();
   return {
     ...actual,
-    sign: vi.fn().mockReturnValue('mocked-access-token')
+    sign: vi.fn().mockReturnValue('access-token')
   }
 })
 
@@ -48,25 +50,41 @@ describe("SignUpController", () => {
   })
 
   // test case 1
-  it("doit creer un utilisateur et retourner un token et un status 200", () => {
+  it("doit creer un utilisateur et retourner un token et un status 200", async () => {
 
     // Arrange
     const name = "jamesjunoir";
     const password = "ABC12345#@";
     const email = "james@test.com";
-    const bodyData = { name, email, password} 
+    const bodyData = { name, email, password}
+    const hashedPassord = "hashed-password93#"
 
     const req = {
       body: bodyData
     } as Request  
 
     // mock
+    vi.mocked(verify).mockReturnValue(true);
+    vi.mocked(hashed).mockResolvedValue(hashedPassord);
+    (User.create as any).mockResolvedValue({
+      name: name,
+      email: email,
+      password: hashedPassord
+    });
+
+    vi.spyOn(JWT, 'sign').mockImplementation((accessToken) => accessToken) as any;
 
 
     // Act
-    //sut.handle(req)
+    const actual = await sut.handle(req);
 
     // Assert
+    expect(actual.status).toBe(200);
+    expect(actual.body).toBe("access-token");
+    expect(User.create).toHaveBeenCalledTimes(1);
+    expect(verify).toHaveBeenCalledTimes(1);
+    expect(verify).toHaveBeenCalledWith(name, password, email)
+
 
   });
   
