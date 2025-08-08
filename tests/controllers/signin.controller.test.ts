6996531e48c520d076aa6b29afb41da795572f25
atemengue <moafembe@gmail.com>
2025-08-08 09:06:06 +0100

    finish test case one signin

--- tests/controllers/signin.controller.test.ts
+++ tests/controllers/signin.controller.test.ts
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import { Request } from 'express';
 import JWT from 'jsonwebtoken';
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import SignInController from '../../src/controllers/signin.controller';
@@ -46,9 +47,35 @@ describe("SignInController", () => {
   describe("handle tests suites", () => {
 
     // test case 01
-    it("doit me retourner Authentication successful avec un status 200 et un token");
+    it("doit me retourner Authentication successful avec un status 200 et un token", async () => {
+
+      // Arrange
+      const name = "antoine-junoir";
+      const password = "testACV136#";
+      const dataReq = { name, password };
+      const token = 'secret-token';
+      const req = {
+        body: dataReq
+      } as Request;
+
+      // Mock Methods
+
+      (User.findOne as any).mockResolvedValue(true);
+      vi.spyOn(bcrypt, 'compare').mockImplementation(() => true);
+      vi.spyOn(JWT, 'sign').mockImplementation(() => token);
+
+      // Act
+      const actual = await sut.handle(req);
+
+      // Assert
+      expect(actual.status).toBe(200);
+      expect(actual.body.token).toBe(token);
+      expect(actual.body.message).toMatch(/successful/i);
+
+
+    });
     // test case 02
-    it.todo("doit me name et password required si les credentials sont incorrects");
+    it.todo("doit me retourner name et password required si les credentials sont incorrects");
     // test case 03
     it.todo("doit retourner user not found si le user n'existe pas");
     // test case 04
