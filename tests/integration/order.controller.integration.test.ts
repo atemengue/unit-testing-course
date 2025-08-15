77631e2196e865299c38951c026c7dc7d42e8d03
atemengue <moafembe@gmail.com>
2025-08-15 11:48:42 +0100

    add cmd and structure of projet

---
+++ tests/integration/order.controller.integration.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from 'vitest';
+
+
+describe("OrderController Tests Suites", () => {
+
+  describe("CreateOrder Tests Suites", () => {
+
+    it.todo("doit creer une commande et retour le status 200 et la commande ");
+
+  })
+
+})
