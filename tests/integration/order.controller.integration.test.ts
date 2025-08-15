f05456d732b068b44316681e55c36045c628bc9c
atemengue <moafembe@gmail.com>
2025-08-15 12:15:43 +0100

    arrange section

--- tests/integration/order.controller.integration.test.ts
+++ tests/integration/order.controller.integration.test.ts
@@ -1,11 +1,50 @@
-import { describe, expect, it } from 'vitest';
+import { Request, Response } from 'express';
+import { describe, expect, it, vi } from 'vitest';
+import orderController from '../../src/controllers/order.controller';
+import Inventory from '../../src/models/inventory';
+import Order from '../../src/models/order';
+import Product from '../../src/models/product';
+import User from '../../src/models/user';
+import { IOrder, OrderStatus } from '../../src/types';
+
 
 
 describe("OrderController Tests Suites", () => {
 
   describe("CreateOrder Tests Suites", () => {
 
-    it.todo("doit creer une commande et retour le status 200 et la commande ");
+    it("doit creer une commande et retour le status 200 et la commande ", async () => {
+
+      // Arrange
+      const user = await User.findOne();
+      const product = await Product.findOne();
+
+      const orderData : IOrder = {
+        userId: user?.id,
+        status: OrderStatus.Created,
+        productId: product?.id,
+        shippingAddress: "Yaounde Mendong",
+        orderDate: new Date(),
+        quantity: 50
+      }
+
+      const req =  {
+        body: orderData
+      } as Request
+
+      const res  = {
+        status: vi.fn().mockReturnThis(),
+        send: vi.fn()
+      } as unknown as Response;
+
+
+      // Act
+      const actual = await orderController.createOrder(req, res)
+
+
+      // Assert
+
+    });
 
   })
 
