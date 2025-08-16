e22a0b36c4836f6bc8e2d2cc5da6d03dd7ff02ce
atemengue <moafembe@gmail.com>
2025-08-16 08:28:07 +0100

    product controller start

---
+++ tests/integration/product.controller.integration.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it, vi } from 'vitest';
+
+describe("Product Controller Integration Tests Suites", () => {
+
+  describe("Create", () => {
+    
+
+    it("doit creer un produit et mettre a jour d'inventaire", () => {
+
+      // Arrange
+
+
+      // Act
+
+
+      // Assert
+
+    })
+
+  })
+
+})
