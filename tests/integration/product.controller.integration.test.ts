21f9c1a4aa1fe96369bc98b4d2116f136656eae1
atemengue <moafembe@gmail.com>
2025-08-16 08:46:49 +0100

    arrange section productMockData

--- tests/integration/product.controller.integration.test.ts
+++ tests/integration/product.controller.integration.test.ts
@@ -1,6 +1,7 @@
 import { afterAll, beforeAll, beforeEach, describe, expect, it, vi } from 'vitest';
 import { seedDatabase, setupTestDB, tearDownTestDB } from '../../src/config/setup';
 import ProductController from '../../src/controllers/product.controller';
+import Category from '../../src/models/category';
 import Product from '../../src/models/product';
 import { InventoryService } from '../../src/services/inventory.service';
 import ProductService from '../../src/services/product.service';
@@ -31,8 +32,19 @@ describe("Product Controller Integration Tests Suites", () => {
 
     it("doit creer un produit et mettre a jour d'inventaire", async () => {
 
+      const coffeChaudCategory = await Category.findOne({
+        name: "Coffee Chaud"
+      });
+
       // Arrange
-      const products = await Product.find();
+      const produtData = {
+        name: "Cafe au Chocolat Chaud",
+        description: "Il est chaud le cafe",
+        price: 750,
+        stock: 8,
+        imageUrl: "image-url",
+        categoryId: coffeChaudCategory?.id
+      }
 
 
       // Act
