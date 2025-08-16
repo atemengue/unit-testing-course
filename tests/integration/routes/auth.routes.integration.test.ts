b087b40e9b124c7b74f11370ce30f51930fb6671
atemengue <moafembe@gmail.com>
2025-08-16 09:47:28 +0100

    tests suites structures

---
+++ tests/integration/routes/auth.routes.integration.test.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import request from 'supertest';
+import { describe, expect, it } from 'vitest';
+
+
+describe("Auth Routes",  () => {
+
+
+  describe("SignUp Tests Suites", () => {
+
+  });
+
+
+  describe("SignIn Tests Suites", () => {
+
+    
+  })
+
+})
