2733103d5c7d8df053bd47f42c80faf01e77f072
atemengue <moafembe@gmail.com>
2025-08-16 10:12:45 +0100

    correct test case 01

--- tests/integration/routes/auth.routes.integration.test.ts
+++ tests/integration/routes/auth.routes.integration.test.ts
@@ -1,6 +1,8 @@
 import express, { Express } from 'express';
 import request from 'supertest';
-import { beforeAll, describe, expect, it } from 'vitest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { seedDatabase, setupTestDB, tearDownTestDB } from '../../../src/config/setup';
+import UserModel from '../../../src/models/user';
 import authRoutes from '../../../src/routes/auth.routes';
 import bodyParser = require('body-parser');
 
@@ -8,15 +10,34 @@ const app : Express = express();
 
 describe("Auth Routes",  () => {
 
-  beforeAll(() => {
+  beforeAll(async () => {
+    await setupTestDB();
+    await seedDatabase();
+
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(authRoutes);
+  });
+
+  afterAll(async() =>{
+    await tearDownTestDB()
   })
 
   describe("SignUp Tests Suites", () => {
 
-    it.todo("doit retourner status 200 avec un token", () => {
+    it("doit retourner status 200 avec un token", async () => {
+      // Arrange
+      const data = {
+        name: "Noah Junoir",
+        password: "1245AFCeft@",
+        email: "noahjunoir@email.com"
+      }
+      // Act &  Assert
+      await request(app).post('/api/signup').send(data).expect(200);
+
+      const users = await UserModel.find();
+
+      expect(users.length).toBe(3);
 
     });
 
@@ -26,9 +47,9 @@ describe("Auth Routes",  () => {
   });
 
 
-  describe("SignIn Tests Suites", () => {
+  // describe("SignIn Tests Suites", () => {
 
     
-  })
+  // })
 
 })
